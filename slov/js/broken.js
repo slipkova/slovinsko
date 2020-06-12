5e3a9487fa2e16126409828ab9144342b3bfc50b
slipkova <46921067+slipkova@users.noreply.github.com>
2020-06-12 10:23:12 +0200

    Add files via upload

---
+++ slov/js/broken.js
@@ -0,0 +1,39 @@
+let imgs = document.getElementsByName("img");
+let srcs = ["img/testo.jpg", "img/done.jpg"];
+
+
+function toggleImg(src1){
+        if(src1 == srcs[0])
+            return srcs[1];
+        else
+            return srcs[0];
+}
+
+$(function(){
+    $(".hidden").toggle();
+    $(".hidden-btn").on("click", ()=>{
+        $("#the-special-one").attr("src", "img/recept.jpg");
+        $("#the-second-specioal-one").attr("src", "img/dot.jpg");
+        $(".hidden").toggle();
+        $(".main").toggle();
+        $("div.nav").css("background-color","purple");
+        let i = 0;
+        setInterval(()=>{
+            for(let element of document.getElementsByClassName("blik")){
+
+                    element.classList.toggle("haha");
+            }
+            if(i > 10){
+                for(let img of imgs){
+                    $(img).attr("src", toggleImg($(img).attr("src")));
+                }
+                i = 0;
+            }
+            i++;
+        }, 50);
+
+
+
+
+    })
+})
---
+++ slov/js/map.js
@@ -0,0 +1,68 @@
+let yellow = "#ffd500";
+let gray = "rgb(204, 204, 204)";
+let active;
+
+function toggleColor(colors, color){
+    if(color == colors[0])
+        return colors[1];
+    else
+        return colors[0];
+}
+
+function mouseover(curr){
+    $(curr).css("fill", toggleColor([gray, yellow], $(curr).css("fill")));
+}
+
+function changeColor(curr, color){
+    $(curr).css("fill", color);
+    $(curr).on("mouseleave", function (){
+        $(curr).css("fill", color);
+    })
+}
+
+$(()=>{
+    let path = "#pol-map g path";
+    let regions = true;
+    $("#regions").css("background-color", yellow);
+
+    $("#regions, #townships").on("click", function(){
+        for(let town of $(path))
+            changeColor(town, gray);
+        $("#regions, #townships").css("background-color", "white");
+        $(this).css("background-color", yellow);
+        if($(this).attr("id") == "regions") regions = true;
+        else regions = false;
+        console.log(regions);
+    })
+
+
+
+    $(path).on("mouseenter mouseleave", function(){
+        if(regions && this.parentElement != active){
+            for(let town of $(this.parentElement.children)){
+                if($(town).attr("fill") != yellow)
+                    mouseover(town);
+            }
+
+        }
+        else
+            mouseover(this);
+    })
+
+    $(path).on("click", function (){
+        for(let town of $(path)){
+            changeColor(town, gray);
+        }
+        if(regions){
+            active = this.parentElement;
+            for(let town of $(this.parentElement.children)){
+                changeColor(town, yellow);
+            }
+            console.log($(active).attr("id"));
+        }else{
+            changeColor(this, yellow);
+            console.log($(this).attr("name"))
+        }
+
+    })
+})
